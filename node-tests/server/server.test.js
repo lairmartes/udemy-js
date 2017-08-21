0be8b275d91e0d8e942015b0096e3f6bacf6dd03
lairmartes <lairmartes@gmail.com>
2017-08-21 09:05:42 -0300

    Including new features in node-tests and starting w/ MongoDB

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,30 +3,34 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world test response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
-});
+describe('Server', () => {
 
-// Make a new test
-// assert 200
-// Assert that you exists in users array
+    describe('GET /', () => {
+        it('should return hello world test response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
 
-it('should include user Lair', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Lair',
-                age: 44
-            });
-        }).end(done);
+    describe('GET /users', () => {
+
+        it('should include user Lair', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Lair',
+                        age: 44
+                    });
+                }).end(done);
+        });
+    });
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,55 +2,55 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
 
-    expect(res).toBe(44).toBeA('number');
-});
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
 
-it('shoud async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (res) => {
-        expect(res).toBe(7).toBeA('number');
-        done();
+            expect(res).toBe(44).toBeA('number');
+        });
+
+        it('shoud async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (res) => {
+                expect(res).toBe(7).toBeA('number');
+                done();
+            });
+        });
     });
-});
 
-it('should square a number', () => {
-    var res = utils.square(5);
+    describe('#square', () => {
+        it('should square a number', () => {
+            var res = utils.square(5);
 
-    expect(res).toBe(25).toBeA('number');
-});
+            expect(res).toBe(25).toBeA('number');
+        });
 
-it('should async square a number', (done) => {
-    utils.asyncSquare(3, (res) => {
-        expect(res).toBe(9).toBeA('number');
-        done();
+        it('should async square a number', (done) => {
+            utils.asyncSquare(3, (res) => {
+                expect(res).toBe(9).toBeA('number');
+                done();
+            });
+        });
     });
-});
 
-//shoud verify first and last names are set
-// asset it includes firstName and lastName with proper values
+    describe('#JSON objects', () => {
+        //shoud verify first and last names are set
+        // asset it includes firstName and lastName with proper values
 
-it('should set first and last names', () => {
-    var user = { location: 'São Paulo', age: 44 };
+        it('should set first and last names', () => {
+            var user = { location: 'São Paulo', age: 44 };
 
-    var res = utils.setName(user, 'Lair Martes');
+            var res = utils.setName(user, 'Lair Martes');
 
-    expect(res).toInclude({
-        firstName: 'Lair',
-        lastName: 'Martes'
+            expect(res).toInclude({
+                firstName: 'Lair',
+                lastName: 'Martes'
+            });
+        });
     });
+});        }).toInclude({
+            age: 44
+        });
+    }); */
 });
-
-/* it('should expect some values', () => {
-    //expect(12).toNotBe(12);
-    //expect({ name: 'lair' }).toNotEqual({ name: 'Lair' });
-    //expect([2, 3, 4]).toExclude(1);
-    expect({
-        name: 'Lair',
-        age: 44,
-        location: 'São Paulo'
-    }).toInclude({
-        age: 44
-    });
-}); */
