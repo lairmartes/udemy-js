38ab957bd22cf4d9f30f7616fb45ccf15a01e240
lairmartes <lairmartes@gmail.com>
2017-08-22 05:57:54 -0300

    Including MongoDB insert and find labs

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
@@ -16,18 +17,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
             console.log(JSON.stringify(result.ops, undefined, 2));
         }); */
+    /* 
+        db.collection('Users').insertOne({
+            name: 'Lair',
+            age: 44,
+            location: 'São Paulo'
+        }, (err, result) => {
+            if (err) {
+                return console.log('Unable to insert user', err);
+            }
 
-    db.collection('Users').insertOne({
-        name: 'Lair',
-        age: 44,
-        location: 'São Paulo'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert user', err);
-        }
-
-        console.log(result.ops);
-    });
+            console.log(result.ops[0]._id.getTimestamp());
+        }); */
 
     db.close();
 });
